ff30da4c72a28e1477bf45702c903e4fc2158446
AP97 <apispati@gmail.com>
2016-03-10 21:08:37 -0600

    Update GolfBalls.cpp

--- RobotMain/GolfBalls.cpp
+++ RobotMain/GolfBalls.cpp
@@ -10,10 +10,16 @@ void GolfBalls::startUp()
 {
   dispenserMotor.attach(dispenserMotorPin);
   scoreServo.attach(scoreServoPin);
+  
+  pinMode(dispenserMotorPin, OUTPUT);
+  digitalWrite(dispenserMotorPin, LOW);
+  pinMode(scoreServoPin, OUTPUT);
+  digitalWrite(scoreServoPin, LOW);
 }
 
 void GolfBalls::periodic(ControllerData ctrl)
 {
-  
+  digitalWrite(dispenserMotorPin, CTRL_DISPENSER_MOTOR? HIGH: LOW);
+  digitalWrite(scoreServoPin, CTRL_GOLF_BALL_SERVO? HIGH: LOW);
 }
 
